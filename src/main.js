40ebdaab6e16fa3d51e7459e1e3b354425f0aedf
MaksimGulevich <max-365@list.ru>
2025-09-15 23:43:02 +0900

    first

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Иконки (если нужны)
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// JS-зависимости (порядок важен)
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+import 'popper.js';
+import 'bootstrap';
+// Твои стили поверх Bootstrap
+import '../styles/main.scss'; // один уровень вверх
+import '../src/style.css'; // один уровень вверх
+import './swiper'
---
+++ src/swiper.js
@@ -0,0 +1,120 @@
+const swiper1 = new Swiper('.swiper1', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+  slidesPerView: 'auto',          // сколько слайдов видно за раз
+  spaceBetween: 30,          // отступы между слайдами (px)
+
+  // And if we need scrollbar
+   scrollbar: {
+    el: '.swiper1-scrollbar',
+    draggable: true,   
+    dragSize: 20,    
+  },
+
+    breakpoints: {
+    300: {
+      spaceBetween: 20,
+      enabled: true,
+    },
+    1200: {
+      spaceBetween: 30,
+      enabled: false,
+    },
+  },
+
+});
+
+
+const swiper2 = new Swiper('.swiper2', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+  slidesPerView: 'auto',          // сколько слайдов видно за раз
+  spaceBetween: 20,          // отступы между слайдами (px)
+
+  // And if we need scrollbar
+   scrollbar: {
+    el: '.swiper2-scrollbar',
+    draggable: true,   
+    dragSize: 20, 
+  },
+
+  // Breakpoints
+  breakpoints: {
+    300: {
+      spaceBetween: 20,
+    },
+    1200: {
+      spaceBetween: 30,
+    },
+  },
+
+});
+
+
+const swiper3 = new Swiper('.swiper3', {
+  // Optional parameters
+  direction: 'horizontal',
+  centeredSlides: true, 
+  slidesPerView: 'auto',          // сколько слайдов видно за раз
+  spaceBetween: 0,  
+  grid: {rows:2, fill: 'row'},
+  // And if we need scrollbar
+   scrollbar: {
+    el: '.swiper3-scrollbar',
+    draggable: true,   
+    dragSize: 20, 
+  },
+ breakpoints: {
+    320: {
+      loop: true,
+      slidesPerView: 2,
+      grid: { rows: 2, },
+    },
+    768: {   
+      loop: true,
+      spaceBetween: 0,                      // если нужно — средний размер
+      slidesPerView: 'auto',
+      grid: {rows:1, fill: 'row'}, // явно ставим 1 строку
+    },
+    1200: {   
+      loop: true,
+      spaceBetween: 0,                      // если нужно — средний размер
+      slidesPerView: 'auto',
+      grid: {rows:1, fill: 'row'}, // явно ставим 1 строку
+    },
+  },
+});
+
+
+
+const swiper4 = new Swiper('.swiper4', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+  slidesPerView: 'auto',          // сколько слайдов видно за раз
+  spaceBetween: 20, 
+
+
+  // Breakpoints
+  breakpoints: {
+    300: {
+      spaceBetween: 0,
+      slidesPerView: 2,
+      grid: { rows: 2, fill: 'row'},
+    },
+     768: {
+      spaceBetween: 20,
+      slidesPerView: 'auto',
+      grid: {rows:1,},
+    },
+    1200: {
+      spaceBetween: 30,
+      slidesPerView: 'auto',
+      grid: {rows:1, fill: 'row'},
+    },
+  },
+
+
+});
